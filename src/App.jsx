e2d307072aa9e04b647c1b79ccaf11c74cd94fae
Sohel Rana <mirzasohel550@gmail.com>
2025-03-03 16:08:42 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+
+const App = () => {
+  return (
+    <div className='bg-[#171d32] h-auto w-full overflow-hidden'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='text-white'>
+        <span>Portfolio</span>
+        <ul>
+            <li>About</li>
+            <li>Experience</li>
+            <li>Projects</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
